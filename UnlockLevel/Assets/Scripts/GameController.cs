6237db2e6805818b47936637de9932af72539287
JoseJunior <jeyjr.dg@gmail.com>
2022-09-26 16:04:04 -0300

    UnlockLevel

---
+++ UnlockLevel/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+
+    public void WinBtn()
+    {
+        if(PlayerPrefs.GetInt("LevelComplete") < SceneManager.GetActiveScene().buildIndex)
+        {
+            PlayerPrefs.SetInt("LevelComplete",SceneManager.GetActiveScene().buildIndex);
+            LoadScene();
+        }
+        else
+        {
+            Debug.Log("Level completado");
+            Invoke("LoadScene", 2f);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ UnlockLevel/Assets/Scripts/levelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class levelManager : MonoBehaviour
+{
+    public Button[] btnLevel;
+    public int BtnActive { get; set ; }
+
+
+    private void Start()
+    {
+        BtnActive = PlayerPrefs.GetInt("LevelComplete");
+        SetEnableLevel(BtnActive);
+    }
+
+    private void SetEnableLevel(int btnValue)
+    {
+        for (int i = btnValue; i < btnLevel.Length; i++)
+        {
+            btnLevel[i].interactable = false;
+        }
+    }
+
+    public void LoadLevel(Button btnName)
+    {
+        SceneManager.LoadScene(btnName.name);
+    }
+
+    public void ZerarSaves()
+    {
+        PlayerPrefs.DeleteAll();
+        SetEnableLevel(0);
+    }
+}
