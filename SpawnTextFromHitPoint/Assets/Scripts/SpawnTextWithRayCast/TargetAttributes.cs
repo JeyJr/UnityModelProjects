66bcc49cfb53cb16a9e6bf416683a51bb0819866
JoseJunior <jeyjr.dg@gmail.com>
2022-09-27 18:52:49 -0300

    TMPro
    - Opção do spawnText utilizando TMPro

--- SpawnTextFromHitPoint/Assets/Scripts/SpawnTextWithRayCast/TargetAttributes.cs
+++ SpawnTextFromHitPoint/Assets/Scripts/SpawnTextWithRayCast/TargetAttributes.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 public class TargetAttributes : MonoBehaviour
 {
     public GameObject txtDmg;
-    public TextMesh txtDmgMesh;
+    //public TextMesh txtDmgMesh;
+    public TextMeshPro txtDmgTMP;
+    
     public Transform txtSpawnPoint;
+
+
+
 
     [SerializeField] private int life = 10000;
     [SerializeField] private int def = 0;
@@ -15,35 +22,39 @@ public class TargetAttributes : MonoBehaviour
     private void Awake()
     {
         def = Random.Range(1, 10);
+        txtDmgTMP = txtDmg.GetComponentInChildren<TextMeshPro>();
     }
+
 
 
     public void TakeDmg(int dmgValue, bool crit)
     {
         if (dmgValue - def < 0)
         {
-            TextSpawn(0);
+           TextSpawn(0, "blue");
         }
         else
         {
             if (crit)
             {
-                txtDmgMesh.color = Color.red;
+                //txtDmgMesh.color = Color.red;
+                TextSpawn(dmgValue - def, "red");
             }
             else
             {
-                txtDmgMesh.color = Color.white;
+                //txtDmgMesh.color = Color.white;
+                TextSpawn(dmgValue - def, "white");
             }
-            TextSpawn(dmgValue - def);
         }
     }
 
-    internal void TextSpawn(int value)
+    internal void TextSpawn(int value, string color)
     {
         Life -= value;
-        txtDmgMesh.text = value.ToString();
+
+        //txtDmgMesh.text = value.ToString();
+        txtDmgTMP.text = $"<color={color}>{value}</color>";
         Instantiate(txtDmg, txtSpawnPoint.position, Quaternion.identity);
-        //Debug.Log($"Name: {this.gameObject.name} | Dano Recebido: {value}");
     }
     
 
